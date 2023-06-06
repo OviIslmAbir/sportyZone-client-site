cc4a24ced7b0fc48b7e2c8345ea4095fb288ffb2
Abir <abirovi481@gmail.com>
2023-06-06 22:02:56 +0600

    3rd commit

--- src/Common/Navbar/Navbar.jsx
+++ src/Common/Navbar/Navbar.jsx
@@ -9,30 +9,30 @@ const Navbar = () => {
     const user = true
     return (
         <div className='container'>
-            <nav class="navbar navbar-expand-lg bg-body-tertiary">
-                <div class="container-fluid">
-                    <a class="navbar-brand" href="#">Sporty Zone</a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+            <nav className="navbar navbar-expand-lg bg-body-tertiary">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">Sporty Zone</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                            <li class="nav-item">
-                              <Link class="nav-link active me-3" to='/'>Home</Link>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                              <Link className="nav-link active me-3" to='/'>Home</Link>
                             </li>
-                            <li class="nav-item">
-                              <Link class="nav-link active me-3" to='/'>Instructors</Link>
+                            <li className="nav-item">
+                              <Link className="nav-link active me-3" to='/'>Instructors</Link>
                             </li>
-                            <li class="nav-item">
-                              <Link class="nav-link active me-3" to='/'>Classes</Link>
+                            <li className="nav-item">
+                              <Link className="nav-link active me-3" to='/'>Classes</Link>
                             </li>
-                            <li class="nav-item">
-                              <Link class="nav-link active me-3" to='/'>Dashboard</Link>
+                            <li className="nav-item">
+                              <Link className="nav-link active me-3" to='/'>Dashboard</Link>
                             </li>
                             <div>
                                 {
-                                    user ? <button onClick={handleLogout}  className='random-btn btn me-3 btn-danger text-dark'>Log out</button>
-                                    : <Link to='/login'><button style={{width:"80px"}} className='random-btn btn btn-danger me-3 text-dark'>Login</button></Link>
+                                    user ? <button onClick={handleLogout}  className='random-btn btn me-3 btn-danger '>Log Out</button>
+                                    : <Link to='/login'><button style={{width:"80px"}} className='random-btn btn btn-danger me-3 '>Login</button></Link>
 
                                 }
                                 {
---
+++ src/Pages/Home/Banner/Banner.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import AwesomeSlider from 'react-awesome-slider';
+import 'react-awesome-slider/dist/custom-animations/fold-out-animation.css';
+import 'react-awesome-slider/dist/styles.css';
+const Banner = () => {
+    return (
+        <div className='container my-4'>
+            <AwesomeSlider style={{height:"550px"}} animation="foldOutAnimation">
+                <div style={{height:"550px"}} data-src="img/banner-2.jpg" />
+                <div style={{height:"550px"}} data-src="img/banner-1.jpg" />
+                <div style={{height:"550px"}} data-src="img/banner-3.jpg" />
+            </AwesomeSlider>
+        </div>
+    );
+};
+
+export default Banner;
