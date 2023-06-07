5989d9f868918867dd6ad810edcdbc48f75960fe
Abir <abirovi481@gmail.com>
2023-06-07 19:01:35 +0600

    8th commit

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -1,15 +1,33 @@
-import React from 'react';
-import AwesomeSlider from 'react-awesome-slider';
-import 'react-awesome-slider/dist/custom-animations/fold-out-animation.css';
-import 'react-awesome-slider/dist/styles.css';
+import React, { useEffect, useRef, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import { Navigation } from "swiper";
+
 const Banner = () => {
+
+    const swiperRef = useRef(null);
+
+    useEffect(() => {
+      const swiperInstance = swiperRef.current.swiper;
+  
+      const interval = setInterval(() => {
+        if (swiperInstance && swiperInstance.slideNext) {
+          swiperInstance.slideNext();
+        }
+      }, 3000);
+  
+      return () => {
+        clearInterval(interval);
+      };
+    }, []);
     return (
         <div className='container my-4'>
-            <AwesomeSlider style={{height:"550px"}} animation="foldOutAnimation" bullets={false}>
-                <div style={{height:"550px"}} data-src="img/banner-2.jpg" />
-                <div style={{height:"550px"}} data-src="img/banner-1.jpg" />
-                <div style={{height:"550px"}} data-src="img/banner-3.jpg" />
-            </AwesomeSlider>
+            <Swiper ref={swiperRef} navigation={true} modules={[Navigation]} className="mySwiper" autoplay loop>
+                <SwiperSlide><img style={{height:"550px", width:"100%"}} src="img/banner-2.jpg" /></SwiperSlide>
+                <SwiperSlide><img style={{height:"550px", width:"100%"}} src="img/banner-1.jpg" /></SwiperSlide>
+                <SwiperSlide><img style={{height:"550px", width:"100%"}} src="img/banner-3.jpg" /></SwiperSlide>
+            </Swiper>
         </div>
     );
 };
